b69e982fbc74e0214fc97dce3f7bc1582b0339ed
JesusDiaz08 <jesus_kdm@hotmail.com>
2018-04-08 19:09:23 -0500

    Even Planning excercise completed

--- Ejercicios/UVA/EventPlanning.cpp
+++ Ejercicios/UVA/EventPlanning.cpp
@@ -7,31 +7,23 @@ int main(){
 	int N,B,H,W;						/*Linea uno: Datos del viaje*/
 	vector<int> minimo;
 	while(cin >> N >> B >> H >> W){		/*N: participantes, B: Presupuesto, H: Hoteles, W: Semanas*/
-		int p = 0;						/*p: precio personal por semana en el i-ésimo hotel*/
-		int a = 0;						/*a: camas disponibles por semana*/
-		bool accept;
 		minimo.clear();
-
 		for (int i = 0; i < H; i++){
+			int p = 0;						/*p: precio personal por semana en el i-ésimo hotel*/
 			cin >> p;				    /*p: precio por persona por semana en el hotel i*/
 			int cost = N*p;
 
-			for (int j = 0; j < W; j++){	
-				accept = true;
+			for (int j = 0; j < W; j++){
+				int a = 0;						/*a: camas disponibles por semana*/	
 				cin >> a;
-				if (a < N){				/*Si el número de habitaciones en menor al de personas*/
-					accept *= false;
-					//continue;
+				if (a >= N){				/*Si el número de habitaciones en menor al de personas*/
+					if (B >= (N*p)){
+						minimo.push_back(cost);
+					}
 				}
 			}//cierra primer for
-
-			if (accept){
-				if (B >= (N*p))
-					minimo.push_back(cost);
-				else
-					accept = false;	
-			}
 		} // cierra segundo for
+
 		sort(minimo.begin() , minimo.end() );
 
 		if (minimo.size()!=0)
