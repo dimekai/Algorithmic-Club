d86a21aaaa0008cbbab396944d16d66cce253a45
JesusDiaz08 <jesus_kdm@hotmail.com>
2018-06-03 23:15:33 -0500

    Area Completed
    Excercise of area completed

--- Ejercicios/UVA/Area.cpp
+++ Ejercicios/UVA/Area.cpp
@@ -16,11 +16,14 @@ int main(){
 	cin >> T;
 	while(T--){
 		cin >> N >> M >> D;
+
+		/*Leemos los datos ingresados*/
 		for (int i = 1; i <= N; i++){
 			for (int j = 1; j <= M; j++)
 				cin >> acum[i][j];
 		}
 
+		/*Hacemos la suma de acumulados*/
 		for (int i = 1; i <= N; i++){
 			for (int j = 1; j <= M; j++)
 				acum[i][j] += acum[i][j-1];
@@ -30,15 +33,24 @@ int main(){
 		}
 
 
+		/*for (int i = 1; i <= N; i++){
+            for (int j = 1; j <=M; j++)
+            	cout << acum[i][j] << " ";
+            cout <<"\n";
+        }*/
+
 		LONG area=0, minCosto=((LONG)1)<<60;
 
+
+		/*Empezamos con el barrido.*/
 		for (int i = 1; i <= N; i++){
-			for (int j = 1;j <= N; j++){
+			for (int j = i;j <= N; j++){
 				LONG tmpArea=0,costo=0;
 				for (int k=1,l=1; l <= M; l++){
 					costo += query(i,j,l,l);
+					//cout << "costo: " << costo << "\n";
 					while(k<=l && costo>D)
-						costo -= query(i,j,k,k++);
+						costo -= query(i,j,k,k),k++;
 					tmpArea = (j-i+1)*(l-k+1);
 					if (tmpArea>area || (tmpArea==area && costo<minCosto))
 						area = tmpArea, minCosto=costo;
