dd29b84aee80032be65688e26458bca01122d36d
JesusDiaz08 <jesus_kdm@hotmail.com>
2018-08-28 00:25:17 -0500

    Excercise of Planetas almost finished.
    Falta describir bien la función de busqueda binaria.

--- Intermedios/Búsqueda Binaria/02_Planetas.cpp
+++ Intermedios/Búsqueda Binaria/02_Planetas.cpp
@@ -1,6 +1,6 @@
 /* 	
-	Author:     Jesús Kaimorts Díaz Medina Date:       
-	Date:		August 25th, 2018.
+	Author:     Jesús Kaimorts Díaz Medina       
+	Date:		August 27th, 2018.
 	Problem:    Planetas 
 	Judge:      OmegaUp 
 	Topic:      Binary Search 
@@ -17,26 +17,54 @@ typedef unsigned short USh;
 typedef double D;
 typedef vector<int> vi;
 
+USh num_planetas = 0;
+vi planetas(MAX_N,0);
+
 /*
 	El análisis de este problema usa la función SystemEquilibrated en donde
 	se calculará el punto óptimo es a partir de dos puntos de referencia: 
 	plantea X y Xi, posicionados a la izquierda y la derecha respectivamente. 
 	El punto de equlibro en el que el meteorito se encuentra, para que el sistema
 	esté en equilibrio,se denomina para esta función Xp.
+
+	El margen de error a contemplar para que el metoro, a pesar de la fuerza
+	(gravitacional) que todos los planetas ejerzan sobre éste, no sea mayor como para
+	poner al sistema inestable. Entonces tenemos que aplicar la ley de conservación
+	de energía para que la suma de todas las fuerzas, ejercidas por los N planetas,
+	sea igual con cero para que el sistema esté estable, para ello usaremos la función
+	balancedForces, la cuál regresará la suma de todas las fuerzas para encontrar el
+	punto estable entre un par de planetas.
+
+	Se está encontrando el punto optimo, en un rango determinado, contemplando los planetas
+	para que el meteorito no colisione con un planeta.
+
 */
-D SystemEquilibrated(int planetaX, int planetaXi){
-	D Xp = 0.0;
-	while(planetaX < planetaXi ){
-		
+
+D balancedForces(D pointM){
+	D sum_forces = 0;
+	for (int i = 0; i < num_planetas ; i++)
+		sum_forces += 1/fabs(planetas[i]-pointM);
+
+	return sum_forces;
+}
+
+
+D systemEquilibrated(int planetaI, int planetaD){
+	D pointEquilibrated = 0.0;
+	while(planetaI <= planetaD){
+		pointEquilibrated = (planetaI + planetaD)/2;
+
+		if(balancedForces(pointEquilibrated) > 0)	/*El sistema está más cargado a la derecha*/
+			planetaD = pointEquilibrated;
+		else 										/*El sistema está más cargado a la izquierda*/
+			planetaI = pointEquilibrated;
 	}
 }
 
 int main(){
-	USh num_planetas = 0;
 	while(cin >> num_planetas){
 		if(num_planetas == 0) break;
 
-		vi planetas(num_planetas,0);
 		for (int i = 0; i < num_planetas; i++)
 			cin >> planetas[i];
 
@@ -45,9 +73,10 @@ int main(){
 		cout << (num_planetas - 1) << "\n";
 
 		for (int  = 1; i < num_planetas; ++i){
-			cout << 
+			cout << fixed << showpoint;
+			cout << setprecision(4);
+			cout << systemEquilibrated(planetas[i-1],planetas[i])
 		}
-
 		planetas.clear();
 	}
 
