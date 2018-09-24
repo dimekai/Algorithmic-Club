54debdf9f480bd8ad618b3271264904238a891ce
JesusDiaz08 <jesus_kdm@hotmail.com>
2018-09-24 00:24:23 -0500

    WCD in process

--- Intermedios/Matemáticas/WCD.cpp
+++ Intermedios/Matemáticas/WCD.cpp
@@ -11,23 +11,24 @@ typedef pair<lli,lli> plli;
 typedef vector<int> vi;
 typedef vector<bool> vb;
 
-vi sievePrime(lli n){
-	cout << "Entra a criba" << "\n";
-	vi primes;
-	vb isPrime;
+vi primes;
+vi isPrime;
 
-	isPrime.resize(n+1, true);
+void sievePrime(){
+	primes.clear();
+	vb isPrime(MAX_COTA+1,true);
+	int cota = sqrt(MAX_COTA)+1;
 	isPrime[0] = false;
 	isPrime[1] = false;
 	primes.push_back(2);
-	for(int j = 4; j<=n ; j+=2) /*Optimización 01*/
+	for(int j = 4; j<=MAX_COTA ; j+=2) /*Optimización 01*/
 		isPrime[j] = false;
-	int cota = sqrt(n);
-	for(int k = 3; k<=n; k+=2){ /*Optimización 02*/
+	
+	for(int k = 3; k<=MAX_COTA; k+=2){ /*Optimización 02*/
 		if(isPrime[k]){
 			primes.push_back(k);
-			if(k <= cota){		/*Optimización 03*/
-				for (int i = k*k; i <= n; i+=2*k)
+			if(k <= cota){			   /*Optimización 03*/
+				for (int i = k*k; i <= MAX_COTA; i+=2*k)
 					isPrime[i] = false;				
 			}
 		}
@@ -39,20 +40,19 @@ int GCD(lli a, lli b){
 	return (b)?GCD(b,a%b):a;
 }
 
-vi factorsOfaNumber(lli numero){
-	cout << "Entra a factNum\n";
-	vi factores(sqrt(numero),0);
-	vi cota_primes = sievePrime(sqrt(numero));
-	for(int i=0; i < cota_primes.size(); i++){
-		cout << "-" << cota_primes[i] << "\n";
-		if(numero%cota_primes[i] == 0){
-			factores[i] = cota_primes[i];
-			numero /= cota_primes[i];
+vi factoresPrimos(lli N){
+	vi factores;
+	int root = sqrt(N);
+
+	for(int i = 2; i <= root ; i++){
+		while(N%i == 0){
+			factores.push_back(i);
+			N /= i;
 		}
 	}
 
-	for(int i=0; i<factores.size(); i++)
-		cout << factores[i] << ",";
+	if(N>1)
+		factores.push_back(N);
 
 	return factores;
 }
